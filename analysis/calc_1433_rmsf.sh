d05d7c14279e1278c253b39bd63fd729032930ff
TyWaldo <tyjwaldo@gmail.com>
2026-09-19 12:01:30 -0700

    Index files

--- analysis/calc_1433_rmsf.sh
+++ analysis/calc_1433_rmsf.sh
@@ -1,6 +1,7 @@
-# Ignore first 10% (200ns) of trajectory
+# Command used to create 14-3-3Z backbone RMSF .xvg files
+# But ran on full trajectories for publication, not truncated ones
+# Ignores first 10% (200ns) of trajectory
 start_time="200000"
 out_file="1433bb_rmsf_start200ns.xvg"
 
-# Provide an index file with a group containing only 14-3-3 backbone atoms
-gmx rmsf -f production_fit.xtc -s production.tpr -n index.ndx -o $out_file -b $start_time -res
+echo "19" | gmx rmsf -f production.xtc -s production.tpr -n index.ndx -o $out_file -b $start_time -res
